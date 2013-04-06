327fd89b4123b9ff0f422b3086ad224ecb7f5452
Andrew Bettison <andrewb@zip.com.au>
2013-04-06 12:11:33 +1030

    Improve account parsing

--- lib/abo/account.py
+++ lib/abo/account.py
@@ -7,29 +7,33 @@
 
 import re
 import abo.text
+from abo.enum import enum
 
-_regex_account_name = re.compile(r'^[A-Za-z0-9_]+$')
-
-def parse_account_name(text):
-    m = _regex_account_name.match(text)
+def parse_account_label(text):
+    m = Account._regex_label.match(text)
     if m:
         return m.group(0)
-    raise ValueError('invalid account name: %r' % (text,))
+    raise ValueError('invalid account label: %r' % (text,))
+
+class AccountType(enum('AssetLiability', 'ProfitLoss', 'Equity')):
+    pass
 
 class Account(object):
 
     r"""Account objects are related hierarchically with any number of root
     Accounts.
 
-    >>> a = Account('a')
-    >>> b = Account('b', parent=a)
-    >>> c = Account('c', parent=b)
-    >>> d = Account('d', parent=a)
-    >>> e = Account('e')
+    >>> a = Account(label='a')
+    >>> b = Account(label='b', parent=a)
+    >>> c = Account(label='c', parent=b)
+    >>> d = Account(label='d', parent=a, atype=AccountType.ProfitLoss)
+    >>> e = Account(label='e')
     >>> c
-    Account('c', parent=Account('b', parent=Account('a')))
+    Account(label='c', parent=Account(label='b', parent=Account(label='a')))
     >>> unicode(c)
     u':a:b:c'
+    >>> d
+    Account(label='d', parent=Account(label='a'), atype=AccountType.ProfitLoss)
     >>> a in c
     False
     >>> c in a
@@ -44,13 +48,19 @@ class Account(object):
 
     """
 
-    def __init__(self, name, parent=None):
-        assert name
+    _rxpat_label = r'[A-Za-z0-9_]+'
+    _regex_label = re.compile('^' + _rxpat_label + '$')
+
+    def __init__(self, name=None, label=None, parent=None, atype=None):
         assert parent is None or isinstance(parent, Account)
-        self.name = name
+        assert name or label
+        self.name = name or label
+        self.label = label
         self.parent = parent
+        self.atype = atype
         self._hash = hash(self.name) ^ hash(self.parent)
         self._children = dict()
+        assert self.name
 
     def __unicode__(self):
         return (unicode(self.parent) if self.parent else u'') + u':' + unicode(self.name)
@@ -59,9 +69,13 @@ class Account(object):
         return (str(self.parent) if self.parent else '') + ':' + str(self.name)
 
     def __repr__(self):
-        r = [repr(self.name)]
+        r = []
+        if self.label is not None:
+            r.append('label=%r' % (self.label,))
         if self.parent is not None:
             r.append('parent=%r' % (self.parent,))
+        if self.atype is not None:
+            r.append('atype=%r' % (self.atype,))
         return '%s(%s)' % (type(self).__name__, ', '.join(r))
 
     def __hash__(self):
@@ -87,52 +101,60 @@ class Chart(object):
     r"""A Chart is a set of accounts.
 
     >>> c = Chart(r'''
-    ... expenses
-    ...   household
-    ...     utilities
-    ...       gas
-    ...       electricity
-    ...       water
-    ...     consumibles
-    ...       food
-    ...     transport
-    ...       car
-    ...       taxi
-    ... income
-    ...     salary
-    ...     rent
-    ...     prizes
+    ... "Expenses"
+    ...   "Household"
+    ...     "Utilities"
+    ...       gas "Gas"
+    ...       elec "Electricity"
+    ...       water "Water usage"
+    ...     "Consumibles"
+    ...       food "Food"
+    ...     "Transport"
+    ...       "Car"
+    ...         car "Car rego, insurance, maintenance"
+    ...         petrol "Petrol for cars"
+    ...       taxi "Taxi journeys"
+    ... "Income"
+    ...     "Salary"
+    ...     rent "Rent"
+    ...     prizes "Prizes"
     ... ''')
     >>> map(unicode, c.accounts()) #doctest: +NORMALIZE_WHITESPACE
-    [u':expenses',
-    u':expenses:household',
-    u':expenses:household:consumibles',
-    u':expenses:household:consumibles:food',
-    u':expenses:household:transport',
-    u':expenses:household:transport:car',
-    u':expenses:household:transport:taxi',
-    u':expenses:household:utilities',
-    u':expenses:household:utilities:electricity',
-    u':expenses:household:utilities:gas',
-    u':expenses:household:utilities:water',
-    u':income',
-    u':income:prizes',
-    u':income:rent',
-    u':income:salary']
+    [u':Expenses',
+     u':Expenses:Household',
+     u':Expenses:Household:Consumibles',
+     u':Expenses:Household:Consumibles:Food',
+     u':Expenses:Household:Transport',
+     u':Expenses:Household:Transport:Car',
+     u':Expenses:Household:Transport:Car:Car rego, insurance, maintenance',
+     u':Expenses:Household:Transport:Car:Petrol for cars',
+     u':Expenses:Household:Transport:Taxi journeys',
+     u':Expenses:Household:Utilities',
+     u':Expenses:Household:Utilities:Electricity',
+     u':Expenses:Household:Utilities:Gas',
+     u':Expenses:Household:Utilities:Water usage',
+     u':Income',
+     u':Income:Prizes',
+     u':Income:Rent',
+     u':Income:Salary']
 
     """
 
     def __init__(self, source_file):
         self.source_file = source_file
         self._accounts = None
+        self._labels = None
 
     def accounts(self):
         if self._accounts is None:
             self._parse(self.source_file)
-        return sorted(self._accounts.itervalues(), key= lambda a: unicode(a))
+        return sorted(self._accounts, key= lambda a: unicode(a))
+
+    _regex_line = re.compile('^(' + Account._rxpat_label + ')?\s*(?:"([^"]+)"|“([^”]+)”)$')
 
     def _parse(self, source_file):
-        self._accounts = {}
+        self._accounts = set()
+        self._labels = {}
         if isinstance(source_file, basestring):
             # To facilitate testing.
             import StringIO
@@ -147,21 +169,22 @@ class Chart(object):
         for line in lines:
             if not line or line.startswith('#'):
                 continue
-            words = line.split()
-            if not words:
-                raise abo.text.LineError('missing account name', line=line)
-            try:
-                name = parse_account_name(words[0])
-            except ValueError, e:
-                raise abo.text.LineError(e, line=line)
+            m = self._regex_line.match(line)
+            if not m:
+                raise abo.text.LineError('malformed line', line=line)
+            label = m.group(1)
+            name = m.group(2)
             assert line.indent <= len(stack)
             if line.indent < len(stack):
                 stack = stack[:line.indent]
-            account = Account(words[0], stack[-1] if stack else None)
-            fullname = unicode(account)
-            if fullname in self._accounts:
-                raise abo.text.LineError('duplicate account name', line=line)
-            self._accounts[fullname] = account
+            account = Account(name=name, label=label, parent= stack[-1] if stack else None)
+            if account in self._accounts:
+                raise abo.text.LineError('duplicate account %r' % unicode(account), line=line)
+            self._accounts.add(account)
+            if account.label:
+                if account.label in self._labels:
+                    raise abo.text.LineError('duplicate account label %r' % (account.label,), line=line)
+                self._labels[account.label] = account
             stack.append(account)
 
 def _test():
