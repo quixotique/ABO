1e2da55a5a5c8322f9591ccc1cf2bed56cb1f673
Andrew Bettison <andrewb@zip.com.au>
2014-01-26 05:59:27 +1030

    Implement --debug and --remove options

--- lib/abo/transaction.py
+++ lib/abo/transaction.py
@@ -32,6 +32,7 @@ u'Them; whatever, else'
 
 import re
 import datetime
+from itertools import chain
 import abo.base
 
 def sign(number):
@@ -66,6 +67,19 @@ class Entry(abo.base.Base):
             r.append(('detail', self.detail))
         return '%s(%s)' % (type(self).__name__, ', '.join('%s=%r' % i for i in r))
 
+    def __eq__(self, other):
+        if not isinstance(other, Entry):
+            return NotImplemented
+        return (self.account == other.account
+            and self.amount == other.amount
+            and self.cdate == other.cdate
+            and self.detail == other.detail)
+
+    def __ne__(self, other):
+        if not isinstance(other, Entry):
+            return NotImplemented
+        return not (self == other)
+
     def _attach(self, transaction):
         """Return an Entry object that is identical to this one, attached to
         the given Transaction object.  Since Entry objects are immutable, if
@@ -88,65 +102,134 @@ class Entry(abo.base.Base):
         """
         return ', '.join([s for s in (self.transaction.description(), self.detail) if s])
 
+    def others(self):
+        """Return an iterator over all other Entries in this Entry's transaction.
+        """
+        assert self.transaction
+        for e in self.transaction.entries:
+            if e is not self:
+                yield e
+
     def replace(self, amount=None):
         """Return a copy of this Entry with new attributes and no transaction.
         """
         return type(self)(None, account=self.account, amount= self.amount if amount is None else amount, detail=self.detail)
 
-    def split(self, amount):
-        """Split this Entry's transaction into two transactions by splitting
-        this Entry into two Entries and dividing all remaining Entries
-        proportionally between the two transactions.
+    def replace_with(self, entry, date=None, split=False):
+        """Replace this Entry in its transaction with another Entry.  The new
+        Entry's amount must have the same sign and must not exceed this entry's
+        amount.
+         - If the amounts are equal and 'split' is False, a new Transaction
+           object is created which contains the new Entry.  If the new Entry
+           was already attached to another Transaction, then returns a copy of
+           the new Entry attached to the new Transaction.
+         - If the amounts are equal and 'split' is True, then two new
+           Transactions are created, the first containing the new entry (or a
+           copy) and the second containing all other entries with the same
+           sign.  All remaining Entries of the opposite sign are divided
+           proportionally between the two transactions.  Returns the two
+           entries.  If there are no other Entries with the same sign, then
+           returns None for the second Entry.
+         - If the amount of the new Entry is less, then two new Transactions
+           are created, the first containing the new Entry (or a copy) and the
+           second containing a copy of this Entry with the amount reduced to
+           the remainder.  All remaining Entries are divided proportionally
+           between the two transactions.  Returns the two entries.
         >>> t1 = Transaction(date=1, who="Someone", what="something",
         ...         entries=({'account':'a1', 'amount':1476, 'detail':'else'},
-        ...                  {'account':'a2', 'amount':500, 'detail':'new'},
-        ...                  {'account':'a3', 'amount':-1020},
+        ...                  {'account':'a2', 'amount':500, 'detail':'blue'},
+        ...                  {'account':'a3', 'amount':-1020, 'detail': 'old'},
         ...                  {'account':'a4', 'amount':-956}))
-        >>> t1.entries[0].split(-1000) #doctest: +NORMALIZE_WHITESPACE
-        (Transaction(date=1, who='Someone', what='something', \
-                     entries=(Entry(account=u'a3', amount=-1000),
-                              Entry(account=u'a2', amount=253, detail=u'new'),
-                              Entry(account=u'a1', amount=747, detail=u'else'))),
-         Transaction(date=1, who='Someone', what='something', \
-                     entries=(Entry(account=u'a4', amount=-956),
-                              Entry(account=u'a3', amount=-20),
-                              Entry(account=u'a2', amount=247, detail=u'new'),
-                              Entry(account=u'a1', amount=729, detail=u'else'))))
+        >>> e = t1.entries[0].replace_with(Entry(None, account='a5', amount=-1000, detail='new'))
+        >>> e #doctest: +NORMALIZE_WHITESPACE
+        (Entry(account=u'a5', amount=-1000, detail=u'new'), Entry(account=u'a3', amount=-20, detail=u'old'))
+        >>> e[0].transaction #doctest: +NORMALIZE_WHITESPACE
+        Transaction(date=1, who='Someone', what='something',
+                    entries=(Entry(account=u'a5', amount=-1000, detail=u'new'),
+                             Entry(account=u'a2', amount=253, detail=u'blue'),
+                             Entry(account=u'a1', amount=747, detail=u'else')))
+        >>> e[1].transaction #doctest: +NORMALIZE_WHITESPACE
+        Transaction(date=1, who='Someone', what='something',
+                    entries=(Entry(account=u'a4', amount=-956),
+                             Entry(account=u'a3', amount=-20, detail=u'old'),
+                             Entry(account=u'a2', amount=247, detail=u'blue'),
+                             Entry(account=u'a1', amount=729, detail=u'else')))
+        >>> e = t1.entries[2].replace_with(Entry(None, account='a5', amount=500, detail='new'))
+        >>> e #doctest: +NORMALIZE_WHITESPACE
+        (Entry(account=u'a5', amount=500, detail=u'new'), None)
+        >>> e[0].transaction #doctest: +NORMALIZE_WHITESPACE
+        Transaction(date=1, who='Someone', what='something',
+                    entries=(Entry(account=u'a3', amount=-1020, detail=u'old'),
+                             Entry(account=u'a4', amount=-956),
+                             Entry(account=u'a5', amount=500, detail=u'new'),
+                             Entry(account=u'a1', amount=1476, detail=u'else')))
+        >>> e = t1.entries[2].replace_with(Entry(None, account='a5', amount=500, detail='new'), split=True)
+        >>> e #doctest: +NORMALIZE_WHITESPACE
+        (Entry(account=u'a5', amount=500, detail=u'new'), Entry(account=u'a1', amount=1476, detail=u'else'))
+        >>> e[0].transaction #doctest: +NORMALIZE_WHITESPACE
+        Transaction(date=1, who='Someone', what='something',
+                    entries=(Entry(account=u'a3', amount=-259, detail=u'old'),
+                             Entry(account=u'a4', amount=-241),
+                             Entry(account=u'a5', amount=500, detail=u'new')))
+        >>> e[1].transaction #doctest: +NORMALIZE_WHITESPACE
+        Transaction(date=1, who='Someone', what='something',
+                    entries=(Entry(account=u'a3', amount=-761, detail=u'old'),
+                             Entry(account=u'a4', amount=-715),
+                             Entry(account=u'a1', amount=1476, detail=u'else')))
         """
-        assert abs(amount) > 0
-        assert abs(amount) < abs(self.amount)
-        assert sign(amount) == self.sign()
+        assert abs(entry.amount) > 0
+        assert sign(entry.amount) == self.sign()
+        assert abs(entry.amount) <= abs(self.amount)
         assert self.transaction is not None
         orig_entries = set(self.transaction.entries)
-        assert self in orig_entries
-        entries1 = [self.replace(amount= amount)]
-        entries2 = [self.replace(amount= self.amount - amount)]
         orig_entries.remove(self)
-        for e in list(orig_entries):
-            if e.sign() == self.sign():
-                entries2.append(e.replace())
-                orig_entries.remove(e)
-        assert orig_entries
-        prop = float(abs(amount)) / float(self.transaction.amount())
-        orig_entries = sorted(orig_entries, key=lambda e: abs(e.amount))
-        total = 0
-        while orig_entries:
-            e = orig_entries.pop(0)
-            assert e.sign() != self.sign()
-            if orig_entries:
-                amt = type(e.amount)(prop * e.amount)
-                total += amt
-                assert abs(total) < abs(amount)
+        if self.amount == entry.amount and not split:
+            orig_entries.add(entry)
+            t1 = self.transaction.replace(entries= orig_entries, date=date)
+            t2 = None
+        else:
+            entries1 = [entry]
+            if self.amount != entry.amount:
+                entry2 = self.replace(amount= self.amount - entry.amount)
+                entries2 = [entry2]
             else:
-                amt = -amount - total
-            assert type(amt) is type(e.amount), 'type(amt)=%r type(e.amount)=%r' % (type(amt), type(e.amount))
-            if amt:
-                entries1.append(e.replace(amount= amt))
-            if amt != e.amount:
-                entries2.append(e.replace(amount= e.amount - amt))
-        t1 = self.transaction.replace(entries= entries1)
-        t2 = self.transaction.replace(entries= entries2)
-        return t1, t2
+                entry2 = None
+                entries2 = []
+            for e in list(orig_entries):
+                if e.sign() == self.sign():
+                    orig_entries.remove(e)
+                    e = e.replace()
+                    entries2.append(e)
+                    if (    entry2 is None
+                        or  (entry2.account != entry.account and e.account == entry.account)
+                        or  abs(e.amount) > abs(entry2.amount)
+                    ):
+                        entry2 = entries2[0]
+            assert orig_entries
+            prop = float(abs(entry.amount)) / float(self.transaction.amount())
+            orig_entries = sorted(orig_entries, key=lambda e: abs(e.amount))
+            total = 0
+            while orig_entries:
+                e = orig_entries.pop(0)
+                assert e.sign() != self.sign()
+                if orig_entries:
+                    amt = prop * e.amount
+                    if type(amt) is not type(e.amount):
+                        amt = type(e.amount)(prop * e.amount)
+                    total += amt
+                    assert abs(total) < abs(entry.amount)
+                else:
+                    amt = -entry.amount - total
+                assert type(amt) is type(e.amount), 'type(amt)=%r type(e.amount)=%r' % (type(amt), type(e.amount))
+                if amt:
+                    entries1.append(e.replace(amount= amt))
+                if amt != e.amount:
+                    entries2.append(e.replace(amount= e.amount - amt))
+            t1 = self.transaction.replace(entries= entries1, date=date)
+            t2 = self.transaction.replace(entries= entries2) if entries2 else None
+        e1 = t1.entries[t1.entries.index(entry)]
+        e2 = t2.entries[t2.entries.index(entry2)] if t2 is not None else None
+        return e1, e2
 
 class Transaction(abo.base.Base):
     """A Transaction is an immutable object that has a date, an optional
@@ -200,10 +283,58 @@ class Transaction(abo.base.Base):
         """
         return '; '.join([s for s in (self.who, self.what) if s])
 
-    def replace(self, entries=None):
+    def replace(self, date=None, entries=None):
         """Return a copy of this Transaction with new attributes.
         """
-        return type(self)(date=self.date, who=self.who, what=self.what, entries= self.entries if entries is None else entries)
+        return type(self)(
+                date= self.date if date is None else date,
+                who= self.who,
+                what= self.what,
+                entries= self.entries if entries is None else entries
+            )
+
+    def split(self, account, amount):
+        """Split this Transaction into two, by splitting all of its Entries on
+        a given account into two sets, the first set summing to a given amount
+        and the second being the remainder.
+        The given amount must have the same sign as the sum of the account's
+        Entries and must not exceed this entry's amount.
+        The Entries are split by dividing each proportionately to create two
+        sets each having the same number of entries (or fewer if any
+        proportional amounts round to zero).
+        >>> t = Transaction(date=1, who="Someone", what="something",
+        ...         entries=({'account':'a1', 'amount':1476, 'detail':'else'},
+        ...                  {'account':'a1', 'amount':500, 'detail':'blue'},
+        ...                  {'account':'a2', 'amount':-1020, 'detail': 'old'},
+        ...                  {'account':'a2', 'amount':-956}))
+        >>> t1, t2 = t.split('a1', 1000)
+        >>> t1 #doctest: +NORMALIZE_WHITESPACE
+        Transaction(date=1, who='Someone', what='something',
+                    entries=(Entry(account=u'a2', amount=-517, detail=u'old'),
+                             Entry(account=u'a2', amount=-483),
+                             Entry(account=u'a1', amount=253, detail=u'blue'),
+                             Entry(account=u'a1', amount=747, detail=u'else')))
+        >>> t2 #doctest: +NORMALIZE_WHITESPACE
+        Transaction(date=1, who='Someone', what='something',
+                    entries=(Entry(account=u'a2', amount=-503, detail=u'old'),
+                             Entry(account=u'a2', amount=-473),
+                             Entry(account=u'a1', amount=247, detail=u'blue'),
+                             Entry(account=u'a1', amount=729, detail=u'else')))
+        """
+        entries = list(self.entries)
+        assert entries
+        total = sum(e.amount for e in entries if e.account == account)
+        assert amount != 0
+        assert sign(amount) == sign(total)
+        assert abs(amount) <= abs(total)
+        if amount == total:
+            return self, None
+        entries1, entries2 = _divide_entries((e for e in self.entries if e.account == account), amount)
+        other1, other2 = _divide_entries((e for e in self.entries if e.account != account), -amount)
+        assert sum(e.amount for e in chain(entries1, entries2, other1, other2)) == 0
+        assert sum(e.amount for e in chain(entries1, entries2) if e.account == account) == total
+        assert sum(e.amount for e in entries1 if e.account == account) == amount
+        return self.replace(entries= entries1 + other1), self.replace(entries= entries2 + other2)
 
     _regex_expand_field = re.compile(r'%{(\w+)([+-]\d+)?}')
 
@@ -216,6 +347,38 @@ class Transaction(abo.base.Base):
                 return d.strftime(r'%-d-%b-%Y')
             return text
         return self._regex_expand_field.sub(repl, text)
+
+def _divide_entries(entries, amount):
+    entries = sorted(entries, key=lambda e: (abs(e.amount), e.account))
+    totale = sum(e.amount for e in entries)
+    total1 = amount
+    entries1 = []
+    entries2 = []
+    while len(entries) > 1:
+        assert abs(total1) < abs(totale)
+        ratio = float(abs(total1)) / float(abs(totale))
+        assert 0 <= ratio <= 1.0
+        e = entries.pop(0)
+        amt = e.amount * ratio
+        if type(amt) is not type(e.amount):
+            amt = type(e.amount)(amt)
+        assert abs(amt) <= abs(e.amount)
+        assert abs(amt) <= abs(total1)
+        if amt != 0:
+            entries1.append(e.replace(amount= amt))
+        if amt != e.amount:
+            entries2.append(e.replace(amount= e.amount - amt))
+        total1 -= amt
+        totale -= e.amount
+    assert len(entries) == 1
+    e = entries[0]
+    assert totale == e.amount
+    if total1:
+        entries1.append(e.replace(amount= total1))
+    if total1 != e.amount:
+        entries2.append(e.replace(amount= e.amount - total1))
+    return entries1, entries2
+
 
 __test__ = {
 'accessors':"""
