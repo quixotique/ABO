49bc9c8fbf260727bd2ad1bf542af35141b873e0
Andrew Bettison <andrew@iverin.com.au>
2022-01-03 01:55:45 +1030

    rabo: Replace Account::parent with Account::children; add AccountIter

--- rabo/src/account.rs
+++ rabo/src/account.rs
@@ -6,45 +6,70 @@ use crate::tags::*;
 
 #[derive(Debug)]
 pub struct Account<'a> {
-    parent: Option<&'a Account<'a>>,
     name: Box<str>,
     currency: &'static Currency,
+    #[allow(dead_code)]
+    children: Vec<&'a Account<'a>>,
     tags: Tags,
 }
 
+#[derive(Debug)]
+pub struct AccountIter<'a> {
+    pub this: &'a Account<'a>,
+    parent: Option<&'a AccountIter<'a>>,
+}
+
 impl<'a> Account<'a> {
-    pub fn new<'b, I: Iterator<Item = &'b str>>(
-        parent: Option<&'a Account<'a>>,
-        name: &'b str,
-        tags: I,
-    ) -> Account<'a> {
+    pub fn new<'b, I: Iterator<Item = &'b str>>(name: &'b str, tags: I) -> Account<'a> {
         Account {
-            parent,
             name: name.to_string().into_boxed_str(),
             currency: DEFAULT_CURRENCY,
+            children: Vec::new(),
             tags: Tags::from_iter(tags),
         }
     }
 
-    pub fn money_from_decimal(&self, amount: Decimal) -> Money {
+    #[allow(dead_code)]
+    fn add_child(&mut self, child: &'a Account<'a>) {
+        self.children.push(child)
+    }
+
+    fn money_from_decimal(&self, amount: Decimal) -> Money {
         Money::from_decimal(amount, self.currency)
     }
 
-    pub fn money_from_str(&self, text: &str) -> Result<Money, MoneyError> {
+    fn money_from_str(&self, text: &str) -> Result<Money, MoneyError> {
         Money::from_str(text, self.currency)
     }
 
     #[allow(dead_code)]
     pub fn write_to(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}{}", self, self.tags)
+        write!(f, "{}{}", self.name, self.tags)
     }
 }
 
-impl fmt::Display for Account<'_> {
+impl<'a> AccountIter<'a> {
+    pub fn new(acc: &'a Account<'a>) -> AccountIter<'a> {
+        AccountIter {
+            this: acc,
+            parent: None,
+        }
+    }
+
+    pub fn money_from_decimal(&self, amount: Decimal) -> Money {
+        self.this.money_from_decimal(amount)
+    }
+
+    pub fn money_from_str(&self, text: &str) -> Result<Money, MoneyError> {
+        self.this.money_from_str(text)
+    }
+}
+
+impl fmt::Display for AccountIter<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         if !self.parent.is_none() {
             write!(f, "{}:", self.parent.unwrap())?;
         }
-        write!(f, "{}", self.name)
+        write!(f, "{}", self.this.name)
     }
 }
