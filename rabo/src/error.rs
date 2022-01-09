58314eb7cd2f99e0f598122577eabb90d49f6367
Andrew Bettison <andrew@iverin.com.au>
2022-01-09 19:26:37 +1030

    rabo: Add error handling

---
+++ rabo/src/error.rs
@@ -0,0 +1,63 @@
+use derive_more::{Constructor, Display, From};
+
+pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+#[derive(Debug, From)]
+#[from(forward)]
+pub struct InputLoc {
+    path: Option<Box<str>>,
+    line_number: u32, // 0 == not known
+}
+
+impl std::fmt::Display for InputLoc {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if !self.path.is_none() {
+            let path = self.path.as_ref().unwrap();
+            if self.line_number != 0 {
+                write!(f, " at {}, line {}", path, self.line_number)
+            } else {
+                write!(f, " in {}", path)
+            }
+        } else if self.line_number != 0 {
+            write!(f, " at line {}", self.line_number)
+        } else {
+            Ok(())
+        }
+    }
+}
+
+impl InputLoc {
+    pub fn new(line_number: u32) -> InputLoc {
+        InputLoc {
+            path: None,
+            line_number,
+        }
+    }
+
+    fn with_path(&self, path: &str) -> InputLoc {
+        InputLoc {
+            path: Some(path.to_string().into_boxed_str()),
+            line_number: self.line_number,
+        }
+    }
+}
+
+#[derive(Constructor, Debug, From, Display)]
+#[from(forward)]
+#[display(fmt = "{}{}", source, loc)]
+pub struct InputError {
+    loc: InputLoc,
+    source: Box<dyn std::error::Error>,
+}
+
+impl std::error::Error for InputError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        Some(&*self.source)
+    }
+}
+impl InputError {
+    pub fn set_path(&mut self, path: &str) -> &InputError {
+        self.loc = self.loc.with_path(path);
+        self
+    }
+}
--- rabo/src/main.rs
+++ rabo/src/main.rs
@@ -1,5 +1,6 @@
 mod account;
 mod date;
+mod error;
 mod money;
 mod tags;
 mod transaction;
@@ -7,7 +8,6 @@ mod transaction;
 use crate::account::*;
 use crate::date::*;
 use crate::transaction::*;
-use std::fs::File;
 use structopt::StructOpt;
 
 #[macro_use]
@@ -56,23 +56,38 @@ struct ReportOpts {
     _labels: bool,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let default_me = "rabo";
+    let me = std::path::Path::new(&args[0])
+        .file_name()
+        .unwrap_or(std::ffi::OsStr::new(&default_me))
+        .to_string_lossy();
     env_logger::init();
+    std::process::exit(match inner() {
+        Ok(_) => 0,
+        Err(err) => {
+            eprintln!("{}: {}", me, err);
+            1
+        }
+    });
+}
 
+fn inner() -> Result<(), Box<dyn std::error::Error>> {
     let opt = Rabo::from_args();
     if opt.debug {
         let mut chart = Chart::new();
-        chart.read_from(File::open("./test/accounts")?)?;
+        chart.read_from_path("./test/accounts")?;
         chart.write_to(&mut std::io::stdout())?;
         chart
             .add_top_level_account(
                 "A1",
                 vec!["labelA1"].into_iter(),
                 vec!["tag11", "tag2"].into_iter(),
-            )
-            .add_child("x", vec!["label_x"].into_iter(), vec!["foo"].into_iter())
-            .add_child("y", vec!["label_y"].into_iter(), vec!["bar"].into_iter());
-        chart.add_top_level_account("A2", vec!["labelA2"].into_iter(), vec![].into_iter());
+            )?
+            .add_child("x", vec!["label_x"].into_iter(), vec!["foo"].into_iter())?
+            .add_child("y", vec!["label_y"].into_iter(), vec!["bar"].into_iter())?;
+        chart.add_top_level_account("A2", vec!["labelA2"].into_iter(), vec![].into_iter())?;
         let t = Transaction::new(
             Date::from_ymd(2021, 12, 29),
             Some(Date::from_ymd(2021, 12, 31)),
@@ -88,7 +103,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 Entry::new(chart.get_account("A2").unwrap(), "-1", None, "detail"),
             ],
             vec!["tag33", "tag4"].into_iter(),
-        );
+        )
+        .unwrap();
         println!("{}", t);
     }
     Ok(())
