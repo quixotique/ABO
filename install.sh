058900c0194ee02a6caa9f24642598342b3b29db
Andrew Bettison <andrew@iverin.com.au>
2022-09-23 21:57:40 +0930

    Add install.sh and MH config files

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+# Bootstrap shellboost.
+if [ ! -f "$SHELLBOOST/libsh/include.sh" -a -r "$HOME/etc/shellboost/libsh/include.sh" ]; then
+   export SHELLBOOST="$HOME/etc/shellboost"
+fi
+. "$SHELLBOOST/libsh/include.sh" || exit $?
+
+__shellboost_include libsh/script.sh || exit $?
+__shellboost_include libsh/install.sh || exit $?
+__shellboost_include libsh/path.sh || exit $?
+
+parse_command_line "$@"
+
+runf cd "${HOME?}"
+link "$here/env/mh_profile"   .mh_profile
+link "$here/env/mhl.headers"  .mhl.headers
